ad1dca8749eeb59f29e201099d5b63cb269e6814
Ivan Storck <ivanoats@gmail.com>
2014-03-27 22:10:29 -0700

    node foreman almost done but flapping server

--- config/auth.js
+++ config/auth.js
@@ -1,12 +1,13 @@
-// config/auth.js
+'use strict';
+var os = require('os');
 
 // expose our config directly to our application using module.exports
 module.exports = {
 
   'twitterAuth' : {
-    'consumerKey'     : '4wa3ODaMwv8LfCAlbob1XQ',
-    'consumerSecret'  : 'V7hSTVcHcjgKngvBmtAKAQTxrbDDOc3fWr8tFopYM',
-    'callbackURL'     : 'http://127.0.0.1:3000/auth/twitter/callback'
+    'consumerKey'     : process.env.TWITTER_CONSUMER_KEY,
+    'consumerSecret'  : process.env.TWITTER_CONSUMER_SECRET,
+    'callbackURL'     : 'http://' + os.hostname() + '/auth/twitter/callback'
   }
 
 };
